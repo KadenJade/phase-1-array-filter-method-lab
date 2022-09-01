e532f4aeed09780ed614bdd0643fe4e29e35ac98
Kaden <kadengraybill@gmail.com>
2022-09-01 17:50:06 -0500

    commit tree 2367dfb79695707fae901e02fab695a06ba95cd3

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+
+
+
+//answer 1
+
+function findMatching(array, str){
+    const driverMatch = array.filter(driver => driver.toLowerCase() === str.toLowerCase())
+    return driverMatch
+}
+
+function fuzzyMatch(array, str){
+ const driverMatch = array.filter(driver => driver.slice(0, str.length) === str);
+ return driverMatch;
+}
+
+function matchName(array, str){
+    const driverMatch = array.filter(driver => driver.name === str)
+    return driverMatch
+}
